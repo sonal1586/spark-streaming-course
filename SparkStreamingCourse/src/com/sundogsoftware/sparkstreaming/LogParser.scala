9471ea47950a24fdbb68dcbf5534ee3a8ee8b73e
Sonal <Sonal@192.168.0.4>
2019-02-03 23:24:43 -0600

    push

--- SparkStreamingCourse/src/com/sundogsoftware/sparkstreaming/LogParser.scala
+++ SparkStreamingCourse/src/com/sundogsoftware/sparkstreaming/LogParser.scala
@@ -1,50 +1,51 @@
+package com.sundogsoftware.sparkstreaming 
 
-package com.sundogsoftware.sparkstreaming
 
-import org.apache.spark.SparkConf
-import org.apache.spark.streaming.{Seconds, StreamingContext}
-import org.apache.spark.storage.StorageLevel
+ import org.apache.log4j._
+ import org.apache.spark.SparkConf
+ import org.apache.spark.streaming._
+ import org.apache.spark.streaming.StreamingContext._
+ import org.apache.spark.streaming.Seconds._
 
-import java.util.regex.Pattern
-import java.util.regex.Matcher
-
-import Utilities._
-
-/** Maintains top URL's visited over a 5 minute window, from a stream
- *  of Apache access logs on port 9999.
- */
-object LogParser {
+ import java.util.regex.Pattern
+ import java.util.regex.Matcher
  
-  def main(args: Array[String]) {
+ import Utilities._
+ import org.apache.spark.storage.StorageLevel
 
-    // Create the context with a 1 second batch size
-    val ssc = new StreamingContext("local[*]", "LogParser", Seconds(1))
-    
-    setupLogging()
-    
-    // Construct a regular expression (regex) to extract fields from raw Apache log lines
-    val pattern = apacheLogPattern()
 
-    // Create a socket stream to read log data published via netcat on port 9999 locally
-    val lines = ssc.socketTextStream("127.0.0.1",1234, StorageLevel.MEMORY_AND_DISK_SER)
-    
-    // Extract the request field from each log line
-    val requests = lines.map(x => {val matcher:Matcher = pattern.matcher(x); if (matcher.matches()) matcher.group(5)})
-    
-    // Extract the URL from the request
-    val urls = requests.map(x => {val arr = x.toString().split(" "); if (arr.size == 3) arr(1) else "[error]"})
-    
-    // Reduce by URL over a 5-minute window sliding every second
-    val urlCounts = urls.map(x => (x, 1)).reduceByKeyAndWindow(_ + _, _ - _, Seconds(5), Seconds(1))
-    
-    // Sort and print the results
-    val sortedResults = urlCounts.transform(rdd => rdd.sortBy(x => x._2, false))
-    sortedResults.print()
-    
-    // Kick it off
-    ssc.checkpoint("/Users/ss889g/checkpoint/")
+ object LorParser {
+   
+   def main(args: Array[String])
+   {
+     setupTwitter()
+     
+     val ssc = new StreamingContext("local[*]","LogParser",Seconds(1))
+     
+     setupLogging()
+     
+     val lines = ssc.socketTextStream("127.0.0.1", 9999, StorageLevel.MEMORY_AND_DISK)
+     
+     val pattern = apacheLogPattern()
+     
+     val request = lines.map(x => {val matcher:Matcher = pattern.matcher(x); if(matcher.matches()) matcher.group(5) })
+     
+     val url = request.map(x => {val arr = x.toString().split(" "); if(arr.length == 3) arr(1) else "[Error]" })
+     
+     val urlCounts = url.map(x => (x,1)).reduceByKeyAndWindow(_ + _, _ - _, Seconds(5), Seconds(1))
+     
+     val sortedResults = urlCounts.transform(rdd => rdd.sortBy(x=>x._2, false))
+     
+     sortedResults.print()
+     
+    ssc.checkpoint("../checkpoint/")
     ssc.start()
     ssc.awaitTermination()
-  }
-}
-
+     
+     
+     
+     
+     
+     
+   }
+ }
